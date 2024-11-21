575b8aa46454d1a9f94ff8b96820800e0b7c5295
Marcus Felix <marcus.felix@gmail.com>
2024-11-21 15:07:51 +0100

    add fetchApi function

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,16 +1,30 @@
+import { parseLocalUser } from "../utils/user";
+
 export default async function fetchApi<T>(options: { url: string, options?: RequestInit, body?: any }): Promise<T> {
   try {
-    const request = await fetch(options.url, {
-      ...options,
-      body: options.body ? JSON.stringify(options.body) : null
-    })
+    const baseUrl = import.meta.env.VITE_SERVER_URL;
+    const body = options.body && options.options?.method !== 'GET' ? JSON.stringify(options.body) : undefined;
+    const token = parseLocalUser()?.token;
 
-    if (!request.ok) {
-      throw new Error(`request failed: status ${request.status}`)
+    if (!token) {
+      throw new Error('missing auth token (session expired?)');
     }
 
-    return await request.json() as T
+    const request = await fetch(`${baseUrl}${options.url}`, {
+      ...options.options,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Auth': token
+      },
+      body: body
+    });
+    const data = await request.json();
+    if (!request.ok) {
+      throw new Error(data.error);
+    }
+
+    return data as T
   } catch (error: any) {
-    throw new Error(`error fetching data: ${error.message}`)
+    throw new Error(error.message);
   }
 }
