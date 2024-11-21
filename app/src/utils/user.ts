77a875a97773acfe20384eb711a48b7bd9de2f55
Marcus Felix <marcus.felix@gmail.com>
2024-11-21 15:04:58 +0100

    add user util functions

--- app/src/utils/user.ts
+++ app/src/utils/user.ts
@@ -1,6 +1,13 @@
 export const parseLocalUser = (): User | null => {
   try {
-    return JSON.parse(sessionStorage.getItem("scheduler-user") ?? "null");
+    const user = JSON.parse(sessionStorage.getItem('scheduler-user') ?? 'null');
+
+    if ((new Date(user.created).getTime() + user.exp) < new Date().getTime) {
+      sessionStorage.removeItem('scheduler-user')
+      return null;
+    }
+
+    return user as User | null;
   } catch (error) {
     return null;
   }
@@ -8,4 +15,16 @@ export const parseLocalUser = (): User | null => {
 
 export const writeLocalUser = (user: User): void => {
   return sessionStorage.setItem('scheduler-user', JSON.stringify(user))
+}
+
+export const parseJwt = (token: string): User => {
+  let base64Url = token.split('.')[1];
+  let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  let jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  const user = JSON.parse(jsonPayload) as User;
+
+  return { ...user, ...{ token: token } };
 }
